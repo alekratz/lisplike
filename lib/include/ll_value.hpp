4bd662739807478de7d17feff670ff1bed5a7085
Alek Ratzloff <alekratz@gmail.com>
2015-10-19 15:50:24 -0400

    Add dict values to ll_value runtime

--- lib/include/ll_value.hpp
+++ lib/include/ll_value.hpp
@@ -1,6 +1,7 @@
 #ifndef LL_VALUE_HPP
 #define LL_VALUE_HPP
 
+#include <map>
 #include <string>
 #include <vector>
 #include <initializer_list>
@@ -24,17 +25,19 @@ enum class ll_value_type
     real,
     fun,
     list,
+    dict,
     none
 };
 
-class ll_value;
-
 typedef const std::string& cstref;
 //typedef std::shared_ptr<ll_value> ll_value_p;
-typedef std::vector<ll_value> ll_value_list;
 
 class ll_value
 {
+public:
+    typedef std::vector<ll_value> ll_value_list;
+    typedef std::map<std::string, ll_value> ll_value_dict;
+
 public:
     ll_value(cstref val)
         : str_val(val)
@@ -51,6 +54,9 @@ public:
     ll_value(std::initializer_list<ll_value> list)
         : list_val(list)
         , type(ll_value_type::list) { }
+    ll_value(ll_value_dict dict)
+        : dict_val(dict)
+        , type(ll_value_type::dict) { }
     ll_value()
         : type(ll_value_type::none) { }
 
@@ -91,6 +97,11 @@ public:
         return real_val;
     }
 
+    ll_value operator[](const std::string& index)
+    {
+        return (dict_val.count(index)) ? dict_val[index] : ll_value();
+    }
+
     operator const char*() const
     {
         assert(type == ll_value_type::str
@@ -109,8 +120,12 @@ public:
     std::string str_val;
     double real_val;
     ll_value_list list_val;
+    ll_value_dict dict_val;
     ll_value_type type;
 };
+
+typedef ll_value::ll_value_list ll_value_list;
+typedef ll_value::ll_value_dict ll_value_dict;
 
 inline bool isnone(const ll_value& val)
 {
