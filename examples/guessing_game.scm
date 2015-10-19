051cfc61ee1caa7758bdb2595144939ff0634e14
Alek Ratzloff <alekratz@gmail.com>
2015-10-19 15:31:42 -0400

    Add a guessing game example, similar to 20 questions

---
+++ examples/guessing_game.scm
@@ -0,0 +1,32 @@
+="ll"
+
+;(coll binary_tree
+;    left
+;    right
+;    trait
+;    animal
+;)
+
+
+(fun ask_q '(tree) (
+        (println "does your animal have %? y/n" tree.trait)
+        (let ans (readln "> "))
+        (if (== ans "y")
+            ; check if right side is none; if so, guess that as the animal
+            (if (isnone tree.right)
+                (println "I guess that your animal is the %" tree.animal)
+                (ask_q tree.right)
+            )
+        ; else if
+        (if (== ans "n")
+            (if (isnone tree.left)
+                (println "I guess that your animal is the %" tree.animal)
+                (ask_q tree.left)
+            )
+        ; else
+        (
+            (println "Please enter 'y' or 'n'")
+            (ask_q tree)
+        )))
+    )
+)
