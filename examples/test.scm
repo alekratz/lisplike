f7732e6b7d978a467c3db633c3a5c11b11d5620f
Alek Ratzloff <alekratz@gmail.com>
2015-10-19 16:11:42 -0400

    Made the example code in test.scm prettier to look at

--- examples/test.scm
+++ examples/test.scm
@@ -14,17 +14,20 @@
     )
 )
 
-;(print "%\n" (factorial 5))
 (println "testing factorials")
 (do_factorials 5)
+
 (println "testing fizzbuzz")
 (fizzbuzz 20)
+
 (println "testing sequences")
 (println "%" (seq 1 2 0.1))
 (println "%" (seq 2 1 -0.1))
+
 (let demo_array '("here" "is" "an" "array" "of" "mixed" "types" 1 2 3 4 5))
 (println "demo array:")
 (println "%" demo_array)
+
 (letm mvar "This is a mutable variable")
 (println "%" mvar)
 (set mvar "And now it has a different value")
